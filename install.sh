3674d99c7708d74ef1115fb142efbf4043f14482
r0w3r <bike123@gmail.com>
2021-07-31 15:26:34 +0200

    install.sh logic update

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #Check and create bin dir in home dir.
-[[ -d ~/bin ]] && mkidir ~/bin && echo "~/bin has been created" || echo "~/bin exist"
+[[ ! -d ~/bin ]] && mkdir ~/bin && echo "~/bin has been created" || echo "~/bin exist"
 
 #Checking and creating symbolic link for ssh_wrap.ssh in ~/bin
-[[ -L ~/bin/ssh_wrap ]] && ln -s "${PWD}"/bin/ssh_wrap.ssh ~/bin/
+[[ ! -L ~/bin/ssh_wrap ]] && ln -s ~/.dotfiles/bin/ssh_wrap.sh ~/bin/ && echo "Symlink to wraper created"
 
 #Checking and creating symbolic links for dotfiles in ~/.
-[[ ! -L .bash.btb ]] && rm ~/.bash.btb && ln -s .bash.btb ../ || echo "File exist"
-[[ ! -L .bash_profile ]] && rm ~/.bash_profile && ln -s .bash_profile ../ || echo "File exist"
-[[ ! -L .bashrc ]] && rm ~/.bashrc && ln -s .bashrc ../ || echo "File exist"
-[[ ! -L .inputrc ]] && rm ~/.inputrc && ln -s .inputrc ../ || echo "File exist"
-[[ ! -L .screenrc ]] && rm ~/.screenrc && ln -s .screenrc ../ || echo "File exist"
+[[ -L .bash.btb ]] && rm ~/.bash.btb && ln -s ~/.dotfiles/.bash.btb ~/ || echo "File exist"
+[[ -L .bash_profile ]] && rm ~/.bash_profile && ln -s ~/.dotfiles/.bash_profile ~/ || echo "File exist"
+[[ -L .bashrc ]] && rm ~/.bashrc && ln -s ~/.dotfiles/.bashrc ~/ || echo "File exist"
+[[ -L .inputrc ]] && rm ~/.inputrc && ln -s ~/.dotfiles/.inputrc ~/ || echo "File exist"
+[[ -L .screenrc ]] && rm ~/.screenrc && ln -s ~/.dotfiles/.screenrc ~/ || echo "File exist"
 
 
