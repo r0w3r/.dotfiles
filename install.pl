8c9561471c10ee4ddf0265a586efa34585616265
r0w3r <bike123@gmail.com>
2023-02-04 12:52:04 +0100

    creating perl ver. of install.sh

--- install.pl
+++ install.pl
@@ -5,12 +5,36 @@ use strict;
 use warnings;
 
 
-#always installing
-#.dotfiles
-#.vim
+sub install_default{
+  print("install\n");
+# - .dotfiles
+# - .bashrc.d
+# - .vim
+}
 
+#install demmand
+sub install_srut{
+  print("srut\n");
+# - srut
+}
 
-#installing on demand
-##starship
-##srut
-###.bashrc.d
+sub install_starship{
+  print("starship\n");
+# - starship
+}
+
+if(defined $ARGV[0] and $ARGV[0] eq 'install'){
+  if(defined $ARGV[1] and $ARGV[1] eq "srut"){
+    install_srut
+  }elsif(defined $ARGV[1] and $ARGV[1] eq "starship"){
+    install_starship
+  }elsif( !defined $ARGV[1]){
+    install_default
+  }
+}elsif( !defined $ARGV[0]){
+  print("Usage:\n");
+  print("\tinstall - to install .dofiles, .vim and .bashrc.d\n");
+  print("\tinstall srut - to install srut alias\n");
+  print("\tinstall starship - to install starship prompt and config\n");
+}
+
